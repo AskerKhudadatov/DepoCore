658b7a3210e3aa06d87bf01ce2b954d2062d55cc
Baku <Baku@DESKTOP-I64A340>
2021-10-22 11:32:15 +0400

    Add project files.

---
+++ Entites/Model/Medicine.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entites.Model
+{
+    class Medicine
+    {
+    }
+}
---
+++ PharmacyApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PharmacyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
